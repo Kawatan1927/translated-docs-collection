96e14457efa1275650086345fc026b5d50c1de1b
kawatan1927 <haruto.kawata@solxyz.co.jp>
2025-02-20 22:39:42 +0900

    初回コミット

---
+++ theme.config.js
@@ -0,0 +1,20 @@
+export default {
+  logo: <span>ドキュメント集(仮)</span>,
+  project: {
+    link: 'https://github.com/kawatan1927',
+  },
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – ドキュメント集(仮)',
+    }
+  },
+  sidebar:{
+    defaultMenuCollapseLevel: 1
+  },
+  feedback: {
+    content: null
+  },
+  editLink: {
+    text: null
+  }
+}
